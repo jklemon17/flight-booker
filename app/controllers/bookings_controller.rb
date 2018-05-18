83fd45fbb6edffe585588db2f503ad69b5318799
PolarisTLX <paul.a.rail@gmail.com>
2018-05-18 14:41:52 -0400

    Booking new, show and create
    Co-authored-by: Kyle Lemon <james.kyle.lemon@gmail.com>

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,8 +6,20 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(params[:booking][:flight_id],params[:booking][:passenger_count])
+
+    if @booking.save
+      @booking.passenger_count.times do
+        passenger = Passenger.new
+      end
+
+      redirect_to @booking
+    else
+
+    end
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'flights#index'
   get 'flights', to: 'flights#index'
 
-  resource :bookings, only: [ :new, :show ]
+  resource :bookings, only: [ :new, :show, :create ]
   # get 'bookings/new'
   # get 'bookings/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
